77cb4da9c5599cb63138add32415b9007968bf18
JRC-Capucho <joaorobertoc.capucho@gmail.com>
2023-11-02 20:17:44 -0300

    New colors solarized osaka

--- nvim/lua/lazy/plugins/lualine.lua
+++ nvim/lua/lazy/plugins/lualine.lua
@@ -1,12 +1,60 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+	dependecies = { "nvim-tree/nvim-we-devicons" },
+	opt = true,
 	config = function()
 		local lualine = require("lualine")
+		local colors = require("solarized-osaka.colors").setup({ transform = true })
+		local config = require("solarized-osaka.config").options
+
+		local solarized_osaka = {}
+
+		solarized_osaka.normal = {
+			a = { bg = colors.blue, fg = colors.black },
+			b = { bg = colors.fg_gutter, fg = colors.blue },
+			c = { bg = colors.bg_statusline, fg = colors.fg_sidebar },
+		}
+
+		solarized_osaka.insert = {
+			a = { bg = colors.green, fg = colors.black },
+			b = { bg = colors.fg_gutter, fg = colors.green },
+		}
+
+		solarized_osaka.command = {
+			a = { bg = colors.yellow, fg = colors.black },
+			b = { bg = colors.fg_gutter, fg = colors.yellow },
+		}
+
+		solarized_osaka.visual = {
+			a = { bg = colors.magenta, fg = colors.black },
+			b = { bg = colors.fg_gutter, fg = colors.magenta },
+		}
+
+		solarized_osaka.replace = {
+			a = { bg = colors.red, fg = colors.black },
+			b = { bg = colors.fg_gutter, fg = colors.red },
+		}
+
+		solarized_osaka.terminal = {
+			a = { bg = colors.green1, fg = colors.black },
+			b = { bg = colors.fg_gutter, fg = colors.green1 },
+		}
+
+		solarized_osaka.inactive = {
+			a = { bg = colors.bg_statusline, fg = colors.blue },
+			b = { bg = colors.bg_statusline, fg = colors.fg_gutter, gui = "bold" },
+			c = { bg = colors.bg_statusline, fg = colors.fg_gutter },
+		}
+
+		if config.lualine_bold then
+			for _, mode in pairs(solarized_osaka) do
+				mode.a.gui = "bold"
+			end
+		end
 		lualine.setup({
 			options = {
 				icons_enabled = true,
-				theme = "solarized_dark",
+				theme = solarized_osaka,
 				section_separators = { left = "", right = "" },
 				component_separators = { left = "", right = "" },
 				disabled_filetypes = {},
@@ -28,7 +76,7 @@ return {
 						symbols = { error = " ", warn = " ", info = " ", hint = " " },
 					},
 					"fileformat",
-					-- "encoding",
+					"encoding",
 					"filetype",
 				},
 				lualine_y = { "progress" },
