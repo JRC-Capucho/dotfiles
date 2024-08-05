b5f01ed753ded31e4ae80b503d5591012d98f08d
JRC-Capucho <joaorobertoc.capucho@gmail.com>
2024-08-05 00:36:49 -0300

    new config for wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,17 +1,19 @@
 local wezterm = require("wezterm")
 
 return {
-	color_scheme = "Catppuccin Mocha",
-	font = wezterm.font_with_fallback({
-		"JetBrains Mono",
-	}),
-	font_size = 10.0,
+	color_scheme = "One Dark (Gogh)",
+	font = wezterm.font("Geist Mono", { weight = "Regular", stretch = "Normal", style = "Normal" }),
+	font_size = 12.0,
 	window_background_opacity = 0.9,
 	enable_tab_bar = false,
 	window_padding = {
 		left = 0,
 		right = 0,
 		top = 0,
-		bottom = 0,
+		bottom = "0.0cell",
 	},
+	underline_thickness = 1,
+	underline_position = -2,
+	max_fps = 60,
+	animation_fps = 30,
 }
