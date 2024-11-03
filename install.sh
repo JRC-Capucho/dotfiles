ed2565eba30bd24177517dccd22e7383fd56e95f
JRC-Capucho <joaorobertoc.capucho@gmail.com>
2024-11-03 12:46:49 -0300

    ...

--- install.sh
+++ install.sh
@@ -8,14 +8,16 @@ source $(pwd)/install/chooses.sh
 
 source $(pwd)/install/terminal.sh
 
-folders="cz,git,zsh,local" 
+folders="cz,git,zsh" 
 for folder in $(echo $folders | sed "s/,/ /g"); do
   stow -D $folder
 done
 stow -D --target=$HOME/.config config
+stow -D --target=$HOME/.local local
 
 stow cz
 stow git
 stow zsh
 stow --target=$HOME/.config config
+stow --target=$HOME/.local local
 stow local
