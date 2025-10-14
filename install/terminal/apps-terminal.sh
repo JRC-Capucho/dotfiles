4a8130ec293439f2aa075747a16449335400b782
JRC-Capucho <joaorobertoc.capucho@gmail.com>
2025-10-13 23:54:24 -0300

    fix packages

--- install/terminal/apps-terminal.sh
+++ install/terminal/apps-terminal.sh
@@ -1,2 +1,1 @@
-yay -S --needed ripgrep bat eza zoxide plocate btop tldr alacritty feh picom rofi nemo stow arand arandr flameshot xclip sshs atac jd git-delta fastfetch toipe base base-devel gnome-disk-utility gnome-keyring cliphist fzy timer-bin glab mpvpaper --noconfirm
-
+yay -S --needed ripgrep bat eza zoxide plocate btop tldr alacritty feh picom rofi nemo stow arand arandr flameshot xclip sshs atac jd git-delta fastfetch toipe base base-devel gnome-disk-utility gnome-keyring cliphist fzy timer-bin glab mpvpaper gd --noconfirm
